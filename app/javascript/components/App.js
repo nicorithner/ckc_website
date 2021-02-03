f67ff0d91d6afb27a1a0620ec47d9af34d43d7bb
Nico Rithner <nicorithner@gmail.com>
2021-02-03 15:49:50 -0700

    Create components folder and App.js

---
+++ app/javascript/components/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+class App extends React.Component {
+				render () {
+						return (
+										<div>
+											<label htmlFor="email"> Email </label>
+											<input type="email" placeholder="enter email address" name="email" />	
+											<label htmlFor="name"> Name </label>
+											<input type="text" placeholder="enter name" name="name"/>	
+											<label htmlFor="message"> Message </label>
+											<textarea placeholder="enter your message here" name="message" rows="4" />	
+											<button type="button">Submit </button>
+
+										</div>
+						)
+				}
+}
+
+export default App
