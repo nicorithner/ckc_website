b13bcd9127133a636ab5f7e71d2acd2e6cf7f07a
Nathan Darrington <npdarrington@icloud.com>
2021-02-08 23:54:35 -0700

    Link application.scss stylesheet

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from '../components/App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../components/App';
+
+import '../stylesheets/application.scss';
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
     <App/>,
     document.body.appendChild(document.createElement('div')),
   )
-})
+});
