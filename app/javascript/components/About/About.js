a9debfcbb4cc7ebd01698d5373b6d09da3e174ab
Nathan Darrington <npdarrington@icloud.com>
2021-02-11 14:15:07 -0700

    Initial structure for About cmp

---
+++ app/javascript/components/About/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './About.scss'
+
+const About = () => (
+  <section className="about-container">
+    <h2>About Colorado Kettlebell Club</h2>
+  </section>
+)
+
+export default About;
